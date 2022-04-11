7ae3b9c5c6600aa680b8ee7389838e4588e7f7f8
rbeale12345 <rbeale@ptaaschool.org>
2022-04-11 11:39:31 -0500

    message_class

--- message_encode.py
+++ message_encode.py
@@ -4,4 +4,7 @@ from typing_extensions import Self
 
 
 class Message():
-    def __init__(): Self 
+    def __init__(self, original_message, offset):
+        self.original = original_message
+        self.off = offset 
+        self.encrypted_message = self.__encode()
